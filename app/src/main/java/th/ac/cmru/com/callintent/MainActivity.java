0a4ee96c8d43768184d12882d35384acdf0ea68b
Theerapan <theerapan_kh@ymail.com>
2016-03-24 09:00:01 +0700

    declare ui+setOnClickListener

--- app/src/main/java/th/ac/cmru/com/callintent/MainActivity.java
+++ app/src/main/java/th/ac/cmru/com/callintent/MainActivity.java
@@ -1,13 +1,36 @@
 package th.ac.cmru.com.callintent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtPhoneNumber = (EditText) findViewById(R.id.phone_number);
+    Button btnMakeCall = (Button) findViewById(R.id.make_call_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtPhoneNumber = (EditText) findViewById(R.id.phone_number);
+        btnMakeCall = (Button) findViewById(R.id.make_call_button);
+        
+        btnMakeCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:"+txtPhoneNumber.getText();
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:12345"));
+                startActivity(intent);
+            }
+        });
     }
 }
