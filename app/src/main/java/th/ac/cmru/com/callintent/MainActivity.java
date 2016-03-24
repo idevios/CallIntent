b12a7316a6a13a27038f4bd21597720795d01701
Theerapan <theerapan_kh@ymail.com>
2016-03-24 09:10:32 +0700

    Check device call ablility

--- app/src/main/java/th/ac/cmru/com/callintent/MainActivity.java
+++ app/src/main/java/th/ac/cmru/com/callintent/MainActivity.java
@@ -1,13 +1,16 @@
 package th.ac.cmru.com.callintent;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
         btnMakeCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                TelephonyManager telephony = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                if(telephony.getPhoneType() == TelephonyManager.PHONE_TYPE_NONE){
+                    String msg = "Sorry, This device can't make phone calls!.";
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 String uri = "tel:"+txtPhoneNumber.getText();
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_DIAL);
